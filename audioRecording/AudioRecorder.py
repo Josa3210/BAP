77c94693d8b9f4b3c515de44964b572531e7385c
J0sa3210 <joeri.winckelmans@outlook.be>
2024-03-26 10:57:06 +0100

    Created small inerface

---
+++ audioRecording/AudioRecorder.py
@@ -0,0 +1,13 @@
+import wave
+import pyaudio
+
+
+class Audiorecorder:
+    def __init__(self, link: str):
+        self.link = link
+
+    def record(self):
+        pass
+
+    def save(self):
+        pass
