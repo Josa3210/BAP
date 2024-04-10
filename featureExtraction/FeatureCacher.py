af2b0615853dd1320a94ae13e5673175f1cc0718
J0sa3210 <joeri.winckelmans@outlook.be>
2024-04-10 15:57:32 +0200

    added clearCache

--- featureExtraction/FeatureCacher.py
+++ featureExtraction/FeatureCacher.py
@@ -3,10 +3,12 @@ from pathlib import Path
 
 import torch
 
+import utils
+
 
 class FeatureCacher:
     def __init__(self, startPath: Path = None):
-        self.cachePath = None
+        self.cachePath: Path = None
         if startPath is not None:
             self.cachePath: Path = startPath.joinpath("cache")
 
@@ -30,8 +32,23 @@ class FeatureCacher:
     def load(self, filePathCache) -> torch.Tensor:
         return torch.load(filePathCache)
 
+    def clearCache(self):
+        if self.cachePath is not None:
+            files = self.cachePath.glob("*.cache")
+            counter = 0
+            for file in files:
+                os.remove(self.cachePath.joinpath(file))
+                counter += 1
+            print(f"Removed {counter} files")
+
     def getCachePath(self, fileName: Path) -> Path | None:
         cacheName: str = fileName.parts[-1].split(".")[0] + ".cache"
         cachePath: Path = self.cachePath.joinpath(cacheName)
 
         return cachePath
+
+
+if __name__ == '__main__':
+    path = utils.getDataRoot().joinpath("testVDB")
+    cacher = FeatureCacher(path)
+    cacher.clearCache()
