1bf30a1f670870a3386b145eefcee5889bb9554b
J0sa3210 <joeri.winckelmans@outlook.be>
2024-04-25 09:08:41 +0200

    Added NeuralNetworkSTFT

--- NeuralNetwork/NeuralNetworks.py
+++ NeuralNetwork/NeuralNetworks.py
@@ -12,8 +12,8 @@ from footstepDataset.FootstepDataset import FootstepDataset
 
 class NeuralNetworkTKEO(InterfaceNN):
 
-    def __init__(self, nPersons: int, sFeatures: int):
-        super().__init__("NeuralNetworkTKEO")
+    def __init__(self, nPersons: int, sFeatures: int, method: nn.init = nn.init.xavier_normal_):
+        super().__init__("NeuralNetworkTKEO", method)
         # Params layer1
         self.l1 = [5, 128, 1]
         # Params layer 2
@@ -51,7 +51,7 @@ class NeuralNetworkTKEO(InterfaceNN):
             nn.AvgPool1d(2, 2),
             nn.Conv1d(in_channels=self.l6[0], out_channels=self.l7[0], kernel_size=self.l7[1], stride=self.l7[2], padding=round(self.l7[1] / 2)),
             nn.ReLU(),
-            nn.AvgPool1d(2, 2),
+            nn.AvgPool1d(2, 2)
         )
 
         # These layers are responsible for classification after being passed through the fLayers
@@ -104,6 +104,81 @@ class NeuralNetworkTKEO(InterfaceNN):
         tSample = sample.unsqueeze(1)
         sample = self.fLayers.forward(tSample)
         print(sample)
+
+
+class NeuralNetworkSTFT(InterfaceNN):
+
+    def __init__(self, nPersons: int, sFeatures: tuple[int, int], initMethod: nn.init = nn.init.xavier_normal_):
+        super().__init__("NeuralNetworkSTFT", initMethod)
+
+        # Params layer1
+        self.l1 = [5, 128, 1]
+        # Params layer 2
+        self.l2 = [5, 128, 1]
+        # Params layer 3
+        self.l3 = [5, 64, 1]
+        # Params layer 4
+        self.l4 = [5, 64, 1]
+        # Params layer 5
+        self.l5 = [5, 64, 1]
+        # Params layer 5
+        self.l6 = [5, 64, 1]
+        # Params layer 5
+        self.l7 = [5, 64, 1]
+
+        # These layers are responsible for extracting features and fixing offsets
+        self.fLayers = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=self.l1[0], kernel_size=self.l1[1], stride=self.l1[2], padding=round(self.l1[1] / 2)),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(in_channels=self.l1[0], out_channels=self.l2[0], kernel_size=self.l2[1], stride=self.l2[2], padding=round(self.l2[1] / 2)),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(in_channels=self.l2[0], out_channels=self.l3[0], kernel_size=self.l3[1], stride=self.l3[2], padding=round(self.l3[1] / 2)),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(in_channels=self.l3[0], out_channels=self.l4[0], kernel_size=self.l4[1], stride=self.l4[2], padding=round(self.l4[1] / 2)),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(in_channels=self.l4[0], out_channels=self.l5[0], kernel_size=self.l5[1], stride=self.l5[2], padding=round(self.l5[1] / 2)),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(in_channels=self.l5[0], out_channels=self.l6[0], kernel_size=self.l6[1], stride=self.l6[2], padding=round(self.l6[1] / 2)),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+            nn.Conv2d(in_channels=self.l6[0], out_channels=self.l7[0], kernel_size=self.l7[1], stride=self.l7[2], padding=round(self.l7[1] / 2)),
+            nn.ReLU(),
+            nn.AvgPool2d(2, 2),
+        )
+
+        # These layers are responsible for classification after being passed through the fLayers
+        self.cInput = self.l7[0] * self.calcInputSize(sFeatures)
+
+        self.cLayers = nn.Sequential(
+            nn.Linear(self.cInput, 1024),
+            nn.ReLU(),
+            nn.Dropout(self.dropoutRate),
+            nn.Linear(1024, 128),
+            nn.ReLU(),
+            nn.Dropout(self.dropoutRate),
+            nn.Linear(128, nPersons),
+            nn.Softmax(dim=1)
+        )
+        self.apply(self.initWeightsZero)
+
+    def calcInputSize(self, nInput):
+        inputSize = nInput
+        for layers in self.children():
+            if isinstance(layers, nn.Sequential):
+                for subLayer in layers.children():
+                    if isinstance(subLayer, nn.Conv1d):
+                        inputSize = self.calcSizeConv(inputSize, filterSize=subLayer.kernel_size[0], stride=subLayer.stride[0], padding=int(subLayer.padding[0]))
+                    if isinstance(subLayer, nn.AvgPool1d):
+                        inputSize = self.calcSizePool(inputSize, filterSize=subLayer.kernel_size[0], stride=subLayer.stride[0], padding=int(subLayer.padding[0]))
+        return inputSize
+
+    def forward(self, x: Tensor):
+        pass
 
 
 if __name__ == '__main__':
