4ad2e993cca65fd12c689a82fe10789a5ab600fe
J0sa3210 <joeri.winckelmans@outlook.be>
2024-04-30 09:55:29 +0200

    Made possibility to visualize the kernels of model

--- NeuralNetwork/VisualizeFilters.py
+++ NeuralNetwork/VisualizeFilters.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
+import torch
+from torch import nn
 from torch.utils.data import DataLoader
 
 import utils
-from NeuralNetwork.NeuralNetworks import NeuralNetworkTKEO
+from NeuralNetwork.NeuralNetworks import NeuralNetworkTKEO, NeuralNetworkTKEO2
 from featureExtraction.FeatureExtractor import FeatureExtractorTKEO
 from footstepDataset.FootstepDataset import FootstepDataset
 
@@ -11,7 +13,7 @@ if __name__ == '__main__':
     filterExtr = FeatureExtractorTKEO()
     filterExtr.noiseProfile = path.joinpath(r"noiseProfile\noiseProfile2.wav")
     participants = ["sylvia", "tine", "patrick", "celeste", "simon"]
-    dataset = FootstepDataset(path, transform=filterExtr, labelFilter=participants, cachePath=utils.getDataRoot().joinpath(r"cache\TKEO4410"))
+    dataset = FootstepDataset(path, transform=filterExtr, labelFilter=participants, cachePath=utils.getDataRoot().joinpath(r"cache\TKEO441"))
     labels = dataset.labelStrings
     batchSize = 4
     dataloader = DataLoader(dataset, batch_size=batchSize, shuffle=True)
@@ -19,12 +21,41 @@ if __name__ == '__main__':
     trainingFeatures, trainingLabels = next(iter(dataloader))
     print(f"Feature batch shape: {trainingFeatures.size()}")
     print(f"Labels batch shape: {trainingLabels.size()}")
-    model = NeuralNetworkTKEO(5, dataset.featureSize)
-    model.loadModel(utils.getDataRoot().joinpath("models/NeuralNetworkTKEO-6078_resample4410.pth"))
+    model = NeuralNetworkTKEO2(5, dataset.featureSize)
+    model.loadModel(utils.getDataRoot().joinpath("models/NeuralNetworkTKEO-new411test2.pth"))
     model.eval()
 
-    out_data = model.fLayers.forward(trainingFeatures.unsqueeze(1))
-    filters = model.l5[0]
+    for child in model.children():
+        if isinstance(child, nn.Sequential):
+            for layer in child.children():
+                if isinstance(layer, nn.Conv1d):
+                    # print(layer.weight)
+                    filters = layer.weight
+                    fmin, fmax = torch.min(filters.data), torch.max(filters.data)
+                    filters = (filters - fmin) / (fmax - fmin)
+                    fig, axs = plt.subplots(filters.size()[1], filters.size()[0])
+                    fig.suptitle("Kernels")
+                    outCh, inCh = filters.size()[0], filters.size()[1]
+                    for i in range(inCh):
+                        for j in range(outCh):
+                            f = filters[j, i, :]
+                            data = f[:]
+                            data = data.detach().numpy()
+
+                            if inCh > 1:
+                                axs[i, j].plot(data)
+                                axs[i, j].set_yticks([])
+                                axs[i, j].set_xticks([])
+                                axs[i, j].set_title(f"Kernel {i},{j}")
+                            else:
+                                axs[j].plot(data)
+                                axs[j].set_yticks([])
+                                axs[j].set_xticks([])
+                                axs[j].set_title(f"Kernel {j}")
+                    plt.show()
+
+    """
+    
     fig, axs = plt.subplots(batchSize, filters)
 
     for i in range(batchSize):
@@ -34,3 +65,4 @@ if __name__ == '__main__':
             axs[i, j].plot(data)
             axs[i, j].set_yticks([])
     plt.show()
+    """
